e72e2649eff12012f8e9b804d512a9e9c28771e8
Da-ja-re <mvaulin2008@ya.ru>
2022-11-15 17:32:42 +0300

    виталя лох 4.0

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi ощ, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 print('Виталя справа лох')
-
+print('вИТАЛЯ ')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
